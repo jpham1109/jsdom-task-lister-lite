5c15de320aa92ae5ec94c25be7a89da47afdf0f3
Jamie H Pham <jpham1109@gmail.com>
2021-03-17 18:35:47 -0400

    Done except for stretches

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+
+const tasks = []
+
+//As a user, I should be able to type a task into the input field.
+//As a user, I should be able to click some form of a submit button.
+//As a user, the task string that I provided should appear on the DOM after the submit button has been activated.
+const form = document.querySelector('form#create-task-form')
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault()
+
+  const id 
+  const description = event.target[0].value
+
+  const newTaskObject = { 
+    id: (tasks.length - 1)+ 1,
+    name: description 
+  }
+
+  function renderTask (newTaskObject) {
+    const li = document.createElement('li')
+    li.dataset.id = newTaskObject.id 
+    li.innerHTML = newTaskObject.name
+
+    const ul = document.querySelector('ul#tasks')
+    
+    ul.appendChild(li)
+  }
+
+  tasks.push(newTaskObject)
+
+  renderTask(newTaskObject)
+  
+  form.reset()
+})
+
+//A delete function that will remove tasks from your list
+
